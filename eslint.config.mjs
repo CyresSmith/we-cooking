4b009a3fd9e68f0648402254ebda4ea399720884
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2025-02-04 12:00:06 +0200

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,78 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { fixupConfigRules } from "@eslint/compat";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+import prettierConfig from "eslint-config-prettier";
+import perfectionist from "eslint-plugin-perfectionist";
+import prettierConfigRecommended from "eslint-plugin-prettier/recommended";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import ts from "typescript-eslint";
+
+import parser from "@typescript-eslint/parser";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const patchedConfig = fixupConfigRules([
+  ...compat.extends("next/core-web-vitals"),
+]);
+
+const config = [
+  ...patchedConfig,
+  ...ts.configs.recommended,
+  ...ts.configs.strict,
+  ...ts.configs.stylistic,
+  ...ts.configs.recommendedTypeChecked,
+  ...ts.configs.stylisticTypeChecked,
+  ...pluginQuery.configs["flat/recommended"],
+  prettierConfig,
+  prettierConfigRecommended,
+  {
+    languageOptions: {
+      parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      perfectionist,
+    },
+    rules: {
+      "perfectionist/sort-array-includes": ["error"],
+      "perfectionist/sort-classes": ["error"],
+      "perfectionist/sort-decorators": ["error"],
+      "perfectionist/sort-enums": ["error"],
+      "perfectionist/sort-exports": ["error"],
+      "perfectionist/sort-heritage-clauses": ["error"],
+      "perfectionist/sort-interfaces": ["error"],
+      "perfectionist/sort-intersection-types": ["error"],
+      "perfectionist/sort-jsx-props": ["error"],
+      "perfectionist/sort-maps": ["error"],
+      "perfectionist/sort-modules": ["error"],
+      "perfectionist/sort-named-exports": ["error"],
+      "perfectionist/sort-named-imports": ["error"],
+      "perfectionist/sort-object-types": ["error"],
+      "perfectionist/sort-objects": ["error"],
+      "perfectionist/sort-sets": ["error"],
+      "perfectionist/sort-switch-case": ["error"],
+      "perfectionist/sort-union-types": ["error"],
+      "perfectionist/sort-variable-declarations": ["error"],
+    },
+    settings: {
+      perfectionist: {
+        type: "alphabetical",
+        partitionByComment: true,
+      },
+    },
+  },
+  { ignores: [".next/*", "typings/*", "*.config.*"] },
 ];
 
-export default eslintConfig;
+export default config;
